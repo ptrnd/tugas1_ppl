941c77a1a032bf647ff38f236104ee4ac3ed254f
ptrnd <putranda.bagus@gmail.com>
2020-09-23 14:31:15 +0700

    major update

--- src/__tests__/helpers.js
+++ src/__tests__/helpers.js
@@ -2,3 +2,39 @@
     Gunakan file ini untuk membuat test case dengan Jest
 */
 
+const {
+    incrementQty,
+    decrementQty,
+    hitungSubtotal,
+    absoluteQty,
+    hitungDiskon,
+    grandTotal
+} = require('../helpers.js')
+
+test('Qty 1 jika ditambah 1 hasilnya 2', () => {
+    expect(incrementQty(1)).toBe(2);
+});
+
+test('Qty 1 jika ditambah "1" hasilnya 2', () => {
+    expect(incrementQty("1")).toBe(2);
+});
+
+test('Qty 2 jika dikurangi 1 hasilnya 1', () => {
+    expect(decrementQty(2)).toBe(1);
+});
+
+test('jika qty -5, maka keluarnya jadi 1', () => {
+    expect(absoluteQty(-5)).toBe(1);
+});
+
+test('Subtotal dari 50000 dn qty 2 adalah 100000', () => {
+    expect(hitungSubtotal(50000, 2)).toBe(100000);
+});
+
+test('Diskon kode KODE25 dari barang berharga 50k dan qty 2 (total 100k) adalah 25000', () => {
+    expect(hitungDiskon(50000, 2, "KODE25")).toBe(25000);
+});
+
+test('Grandtotal dari barang dengan harga 50000 qty 2 diskon KODE25 adalah 75000', () => {
+    expect(grandTotal(50000, 2, "KODE25")).toBe(75000);
+});
--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,56 @@
-const incrementQty = (qty) => qty + 1;
-const decrementQty = (qty) => qty - 1;
+const incrementQty = (qty) => parseInt(qty) + 1;
+const decrementQty = (qty) => parseInt(qty) - 1;
 
-module.exports = { 
-    incrementQty, 
-    decrementQty
+function hitungSubtotal(price, qty) {
+    return price * qty;
+}
+
+function grandTotal(price, qty, kode) {
+    return hitungSubtotal(price, qty) - hitungDiskon(price, qty, kode);
+}
+
+function absoluteQty(qty) {
+    return (parseInt(qty) < 1) ? 1 : qty;
+}
+
+function hitungDiskon(price, qty, kode) {
+    const subTotal = hitungSubtotal(price, qty);
+    switch (kode) {
+        case "KODE25":
+            hasilDiskon = subTotal * 0.25;
+            break;
+        case "KODE10":
+            hasilDiskon = subTotal * 0.10;
+            break;
+        case "KODE50":
+            hasilDiskon = subTotal * 0.50;
+            break;
+        case "PELANGGAN1JT":
+            hasilDiskon = subTotal * 0.40;
+            break;
+        default:
+            hasilDiskon = 0;
+            break;
+    }
+    // if (kode == "KODE25") {
+    //     hasilDiskon = subTotal * 0.25;
+    // } else if (kode == "KODE10") {
+    //     hasilDiskon = subTotal * 0.10;
+    // } else if (kode == "KODE50") {
+    //     hasilDiskon = subTotal * 0.50;
+    // } else if (kode == "PELANGGAN1JT") {
+    //     hasilDiskon = subTotal * 0.40;
+    // } else {
+    //     hasilDiskon = 0;
+    // }
+    return hasilDiskon;
+}
+
+module.exports = {
+    incrementQty,
+    decrementQty,
+    hitungSubtotal,
+    absoluteQty,
+    hitungDiskon,
+    grandTotal
 };
